6af89d793f21cdb7c7ba7f89a9cc560931acd805
bapiraju1234 <112751566+bapiraju1234@users.noreply.github.com>
2022-10-24 10:46:06 +0530

    num = int(input(""))
    #find number of digits in num 
    n = len(str(num))
    
    #compute square
    sqr = num**2
    
    #Extract last n digits from the square
    last = sqr%pow(10,n)
    
    #compare last n digits with the input number
    if last == num:
      print("Automorphic Number")
    else:
      print("Not an Automorphic Number")

---
+++ Automorphic_Number.py
@@ -0,0 +1,16 @@
+num = int(input(""))
+
+#find number of digits in num 
+n = len(str(num))
+
+#compute square
+sqr = num**2
+
+#Extract last n digits from the square
+last = sqr%pow(10,n)
+
+#compare last n digits with the input number
+if last == num:
+  print("Automorphic Number")
+else:
+  print("Not an Automorphic Number")
