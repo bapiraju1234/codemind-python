1f8d161641d5981d5e5cc522ecb0f874514f919c
bapiraju1234 <112751566+bapiraju1234@users.noreply.github.com>
2022-10-24 10:31:28 +0530

    def reverse (n) :     rev = 0     while n>0:         r = n%10         rev = rev*10+r         n = n//10     return(rev) a = int(input()) b = a**2 c = reverse(a) d = c**2 e = reverse(d) if (b==e):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,16 @@
+def reverse (n) :
+    rev = 0
+    while n>0:
+        r = n%10
+        rev = rev*10+r
+        n = n//10
+    return(rev)
+a = int(input())
+b = a**2
+c = reverse(a)
+d = c**2
+e = reverse(d)
+if (b==e):
+    print("True")
+else:
+    print("False")
